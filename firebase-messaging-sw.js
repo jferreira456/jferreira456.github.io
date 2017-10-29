ee256c72423eb5b64ac35638ecafd46a7f9a9ae2
jferreira456 <jferreira@e-goi.com>
2017-10-29 23:35:37 +0000

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,16 +5,9 @@ importScripts('/__/firebase/3.9.0/firebase-app.js');
 importScripts('/__/firebase/3.9.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
-const messaging = firebase.messaging();
+//const messaging = firebase.messaging();
 
-/**
- * Here is is the code snippet to initialize Firebase Messaging in the Service
- * Worker when your app is not hosted on Firebase Hosting.
 
- // [START initialize_firebase_in_sw]
- // Give the service worker access to Firebase Messaging.
- // Note that you can only use Firebase Messaging here, other Firebase libraries
- // are not available in the service worker.
  importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-app.js');
  importScripts('https://www.gstatic.com/firebasejs/3.9.0/firebase-messaging.js');
 
@@ -28,7 +21,7 @@ const messaging = firebase.messaging();
  // messages.
  const messaging = firebase.messaging();
  // [END initialize_firebase_in_sw]
- **/
+ 
 
 
 // If you would like to customize notifications that are received in the
